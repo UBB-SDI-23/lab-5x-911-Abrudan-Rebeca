1f84ef16d149500ad1e241b296639efc9cf61768
911-Rebeca_Abrudan <91695625+911-Abrudan-Rebeca@users.noreply.github.com>
2023-04-02 23:40:01 +0300

    frontend

---
+++ Books/lab1-frontend/src/components/statistics/PublisherAvgBooksStars.tsx
@@ -0,0 +1,84 @@
+import {
+	TableContainer,
+	Paper,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	CircularProgress,
+	Container,
+	IconButton,
+	Tooltip,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { Publisher } from "../../models/Publisher";
+import ReadMoreIcon from "@mui/icons-material/ReadMore";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import AddIcon from "@mui/icons-material/Add";
+import { GlobalURL } from "../../constants";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Statistic {
+	id: number;
+	Publisher: string;
+	avg_stars: number;
+	book_count: number;
+  }
+export const PublisherAvgBooksStars = () => {
+    const [statistics, setStatistics] = useState<Statistic[]>([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+	setLoading(true);
+	axios.get(`${GlobalURL}/publisher/statistics/`)
+	  .then(response => {
+		setStatistics(response.data);
+		setLoading(false);
+	  })
+	  .catch(error => {
+		console.error(error);
+		setLoading(false);
+	  });
+  }, []);
+
+  return (
+	<Container>
+		<h1>Average Books Stars Publisher</h1>
+		<p>Here we have publishers ordered by the average of their books stars</p>
+		{loading && <CircularProgress />}
+		{!loading && statistics.length === 0 && <p>No statistic found</p>}
+		
+		{!loading && statistics.length > 0 && (
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>#</TableCell>
+							<TableCell >Publisher</TableCell>
+							<TableCell align="right">Avg_Stars</TableCell>
+							<TableCell align="right">BooksCount</TableCell>
+						   
+						
+						</TableRow>
+					</TableHead>
+					<TableBody>
+				{statistics.map((statistic, index) => (
+					<TableRow key={statistic.id}>
+						<TableCell>{index + 1}</TableCell>
+						<TableCell>{statistic.Publisher}</TableCell>
+						<TableCell align="right">{statistic.avg_stars}</TableCell>
+						<TableCell align="right">{statistic.book_count}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+						
+				</Table>
+			</TableContainer>
+		)}
+	</Container>
+);
+};
+
+//export default AverageProdYear;
