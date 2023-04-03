c64a54cacaa7d2ecefa4bde4762604e446cb8fe8
911-Rebeca_Abrudan <91695625+911-Abrudan-Rebeca@users.noreply.github.com>
2023-04-03 14:23:16 +0300

    sorting

--- Books/lab1-frontend/src/components/statistics/PublisherAvgBooksStars.tsx
+++ Books/lab1-frontend/src/components/statistics/PublisherAvgBooksStars.tsx
@@ -29,10 +29,12 @@ interface Statistic {
 	avg_stars: number;
 	book_count: number;
   }
+
 export const PublisherAvgBooksStars = () => {
     const [statistics, setStatistics] = useState<Statistic[]>([]);
     const [loading, setLoading] = useState(false);
- 
+	const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc'); // initial sort order is ascending
+
 	useEffect(() => {
 		setLoading(true);
 		axios.get(`${GlobalURL}/publisher/statistics/`)
@@ -46,6 +48,19 @@ export const PublisherAvgBooksStars = () => {
 		});
     }, []);
 
+	
+	const handleSortOrderChange = () => {
+		setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); // toggle sort order between ascending and descending
+	  };
+	
+	  const sortedStatistics = statistics.sort((a, b) => {
+		if (sortOrder === 'asc') {
+		  return a.avg_stars - b.avg_stars;
+		} else {
+		  return b.avg_stars - a.avg_stars;
+		}
+	  });
+
   return (
 	<Container>
 		<h1>Average Books Stars Publisher</h1>
@@ -54,34 +69,36 @@ export const PublisherAvgBooksStars = () => {
 		{!loading && statistics.length === 0 && <p>No statistic found</p>}
 		
 		{!loading && statistics.length > 0 && (
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label="simple table">
-					<TableHead>
-						<TableRow>
-							<TableCell>#</TableCell>
-							<TableCell >Publisher</TableCell>
-							<TableCell align="right">Avg_Stars</TableCell>
-							<TableCell align="right">BooksCount</TableCell>
-						   
-						
-						</TableRow>
-					</TableHead>
-					<TableBody>
-				{statistics.map((statistic, index) => (
-					<TableRow key={statistic.id}>
-						<TableCell>{index + 1}</TableCell>
-						<TableCell>{statistic.publisher}</TableCell>
-						<TableCell align="right">{statistic.avg_stars}</TableCell>
-						<TableCell align="right">{statistic.book_count}</TableCell>
-					</TableRow>
-				))}
-			</TableBody>
-						
-				</Table>
-			</TableContainer>
-		)}
-	</Container>
-);
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>CarBrand</TableCell>
+                <TableCell align="right">
+                  <Tooltip title="Sort by Average Stars" arrow>
+                    <IconButton onClick={handleSortOrderChange}>
+                      {sortOrder === 'asc' ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
+                    </IconButton>
+                  </Tooltip>
+                  Avg_Production_Year
+                </TableCell>
+                <TableCell align="right">Car_Count</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sortedStatistics.map((statistic, index) => (
+                <TableRow key={statistic.id}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{statistic.publisher}</TableCell>
+                  <TableCell align="right">{statistic.avg_stars}</TableCell>
+                  <TableCell align="right">{statistic.book_count}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </Container>
+  );
 };
-
-//export default AverageProdYear;
